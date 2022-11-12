8b8fda40b2e89cf66e1b80d633da51012b144d86
june <mailforjavascript@gmail.com>
2022-11-12 23:27:10 +0700

    component

---
+++ src/frontend/src/utils/data-formater.js
@@ -0,0 +1,21 @@
+export default () => ({
+	shortDate(date){
+
+		console.log(date)
+		if(!date){
+			return ""
+		}
+
+		let result = date.getFullYear() + '-' +
+     ( (date.getMonth() > 9) ? date.getMonth() : ("0" + date.getMonth()) ) + '-' +
+     ( (date.getDate() > 9)  ? date.getDate()  : ("0" + date.getDate())  )
+
+		return result;
+	},
+	curentDate(){
+		let now = new Date();
+		  
+		return shortDate(now);
+	}
+})
+
