2220a25fcaa71f47f5d98f4ab7b5278685e97d37
june <mailforjavascript@gmail.com>
2022-11-28 19:28:19 +0700

    refactor: requests api, store

--- src/main/java/kir/tm/v_1/controller/PersonController.java
+++ src/main/java/kir/tm/v_1/controller/PersonController.java
@@ -10,7 +10,6 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -32,7 +31,7 @@ public class PersonController {
 
         }
     }
-    // @CrossOrigin(origins = "http://localhost:8080/api/v1/persons/add" )
+    // @CrossOrigin(origins = "http://localhost:8080/api/v1/persons/add" 
     
     @PostMapping
     @CrossOrigin(origins = "/**")
