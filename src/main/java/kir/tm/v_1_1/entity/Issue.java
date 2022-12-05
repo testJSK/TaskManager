1cc769ee4144539c4c39b36ac09ff94eb95b2594
june <mailforjavascript@gmail.com>
2022-12-05 19:48:00 +0700

    refactor: db

--- src/main/java/kir/tm/v_1_1/entity/Issue.java
+++ src/main/java/kir/tm/v_1_1/entity/Issue.java
@@ -11,7 +11,7 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
-@Table(name = "issues", schema = "dbo")
+@Table(name = "issues", schema = "public")
 public class Issue implements Serializable {
 
 	@Id 	@Column(name = "issue_id") 	@GeneratedValue(strategy = GenerationType.IDENTITY)
