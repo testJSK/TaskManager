7c841c44f0aefc393eeb0908bf89295de537f312
june <mailforjavascript@gmail.com>
2022-12-22 16:37:02 +0700

    refactor: db

--- src/main/resources/db/migration/V2__ADD_DESC_COL.sql
+++ src/main/resources/db/migration/V2__ADD_DESC_COL.sql
@@ -2,5 +2,4 @@
 -- alter table public.tasks add column status_id int4 null
 -- alter table public.tasks add column price int4 null
 -- ALTER TABLE public.tasks RENAME COLUMN parent_task_id TO parent_id;
-
 -- INSERT INTO dbo.issues (issue_id, title) values (1, '====-- flyway insert v2 =========');
